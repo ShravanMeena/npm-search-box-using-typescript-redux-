2640a987f4d513595749143ccefeaa983004d0b7
shravan meena <shravanmeena@shravans-MacBook-Pro.local>
2022-02-01 02:01:29 +0530

    Test

--- src/projects/searchnpm/index.tsx
+++ src/projects/searchnpm/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useAction } from "../../hooks/useActions";
 import { useTypedSelector } from "../../hooks/useTypeSelector";
-import { actionCreators } from "../../redux";
 
 const SearchNpm: React.FC = () => {
   const [term, setTerm] = useState<string>("");
@@ -19,30 +18,36 @@ const SearchNpm: React.FC = () => {
     <div>
       <form onSubmit={submitHandler}>
         <input
+          className="searchInput"
           type="text"
           name="name"
           value={term}
           onChange={(event) => setTerm(event.target.value)}
         />
-        <button>Search</button>
+        <button className="btnBox">Search</button>
       </form>
       {error && error}
       {loading && "Loading..."}
-      <div
-        style={{
-          height: "80vh",
-          width: "500px",
-          overflow: "scroll",
-          marginTop: 40,
-          border: "1px solid #ccc",
-        }}
-      >
-        {!loading && !error && (
-          <>
-            {data.map((name) => {
-              return <p key={name}>{name}</p>;
-            })}
-          </>
+      <div style={{ height: "80vh" }}>
+        {!loading && !error && data.length !== 0 && (
+          <div
+            style={{
+              height: "100%",
+              width: "680px",
+              overflow: "scroll",
+              marginTop: 40,
+              border: "1px solid #ccc",
+              textAlign: "left",
+              padding: "5px 20px",
+              borderRadius: 5,
+            }}
+          >
+            <>
+              {data.map((name) => {
+                return <p key={name}>npm install {name}</p>;
+              })}
+            </>
+          </div>
         )}
       </div>
     </div>
